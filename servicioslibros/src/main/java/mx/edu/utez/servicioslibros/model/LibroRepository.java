9edc012028bb0d796e5bcc85c03aeaba5874d407
Tlacuano <20213tn105@utez.edu.mx>
2024-02-29 16:11:14 -0600

    peticiones

--- servicioslibros/src/main/java/mx/edu/utez/servicioslibros/model/LibroRepository.java
+++ servicioslibros/src/main/java/mx/edu/utez/servicioslibros/model/LibroRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface LibroRepository extends JpaRepository<BeanLibro, Long>{
     Page<BeanLibro> findAll(Pageable pageable);
+    Page<BeanLibro> findByOrderByAutorDesc (Pageable pageable);
+    Page<BeanLibro> findByOrderByFechaPublicacionDesc (Pageable pageable);
+    Page<BeanLibro> findByPortadaIsNotNull(Pageable pageable);
 }
